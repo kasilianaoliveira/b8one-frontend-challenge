fd0da2450d94af23a1f715f9992d3a126bb0bfbc
kasilianaoliveira <kasilianaoliveira@gmail.com>
2024-10-09 20:09:56 -0300

    feat: add toast when clicking favorite button

--- src/components/FavoriteIcon/index.tsx
+++ src/components/FavoriteIcon/index.tsx
@@ -1,19 +1,26 @@
-import { HeartFilled, HeartOutlined } from "@ant-design/icons"
-import { FC } from "react";
+import { HeartFilled, HeartOutlined } from '@ant-design/icons'
+import { FC, useContext } from 'react';
 import styles from './styles.module.css'
-import { FavoriteProductCardProps } from "../../types/favoriteProduct";
+import { IFavoriteProductCardProps } from '../../types/favoriteProduct';
+import { FavoriteContext } from '../../context/favoriteContext';
+import { notification } from '../../utils/notification';
 
-export const FavoriteIcon: FC<FavoriteProductCardProps> = ({ product, favorites, handleFavoriteProduct }) => {
-
+export const FavoriteIcon: FC<IFavoriteProductCardProps> = ({ product, favorites, handleFavoriteProduct }) => {
+  const { removeProductFromFavorites } = useContext(FavoriteContext)
   const isProductFavorited = (productId: string) => {
     return favorites.some((item) => item.id === productId);
   }
+
+
   return (
     <>
       {
         isProductFavorited(product.id) ? (
           <HeartFilled
-            onClick={() => handleFavoriteProduct(product)}
+            onClick={() => {
+              removeProductFromFavorites(product.id)
+              notification('Produto removido dos favoritos!')}
+            }
             style={{
               fontSize: '21px',
 
@@ -25,7 +32,10 @@ export const FavoriteIcon: FC<FavoriteProductCardProps> = ({ product, favorites,
 
         ) : (
           <HeartOutlined
-            onClick={() => handleFavoriteProduct(product)}
+            onClick={() => {
+              handleFavoriteProduct(product)
+              notification('Produto adicionado aos favoritos!')
+            }}
             style={{ fontSize: '21px' }}
             className={
               `${styles['favorite-icon']} ${styles['favorite-icon--outlined']}`
