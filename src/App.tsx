30b0d573c5642121cdec4d7ed5015d580b8315b3
kasilianaoliveira <kasilianaoliveira@gmail.com>
2024-10-09 20:13:47 -0300

    feat: add components and context providers

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,38 @@
+import { ToastContainer } from "react-toastify"
+import { Cart } from "./components/Cart"
+import { Favorite } from "./components/Favorite"
+import { Header } from "./components/Header"
 import { Products } from "./components/Products"
+import { CartContextProvider } from "./context/cartContext"
+import { FavoriteContextProvider } from "./context/favoriteContext"
 import './global.css'
+import 'react-toastify/dist/ReactToastify.css';
 function App() {
 
   return (
-    <main className='App'>
-      <Products/>
-    </main>
+    <CartContextProvider>
+      <FavoriteContextProvider>
+        <main className='App'>
+          <Header />
+          <Favorite />
+          <Cart />
+          <Products />
+          <ToastContainer
+            position="bottom-right"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="light"
+          />
+        </main>
+      </FavoriteContextProvider>
+    </CartContextProvider>
+
   )
 }
 
