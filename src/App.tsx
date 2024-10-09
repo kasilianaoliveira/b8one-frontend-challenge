cddb5cab444045df6a372f5e2b0c1a5e5c2810c8
kasilianaoliveira <kasilianaoliveira@gmail.com>
2024-10-09 07:51:03 -0300

    refactor: update App.tsx and main.tsx for improved functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Products } from "./components/Products"
+import './global.css'
 function App() {
 
   return (
-    <div className='App'>
-      <h1>Hello</h1>
-    </div>
+    <main className='App'>
+      <Products/>
+    </main>
   )
 }
 
